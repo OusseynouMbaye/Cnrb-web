b3d50d54f49fe8315a10f1be4a7c61796c3671b0
Ousseynou Mbaye <ouzinem@hotmail.com>
2021-10-30 13:01:00 -0400

    Header page and Home Page Load

---
+++ crnb-web-site/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+ <header>
+     <ul>
+         <li>Home</li>
+         <li>Club</li>
+         <li>Competition</li>
+     </ul>
+     
+ </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    
+}
+</script>
+<style scoped>
+
+header{
+    display: flex;
+}
+</style>
